4b28c0ef1d06b4f1905620dc8c5a1a7e5499c239
Sai Bharath Kumar Bandi <b20bharath@gmail.com>
2021-04-15 16:33:31 -0500

    click functionality of navigation bar

--- Application/Components/Navigate.js
+++ Application/Components/Navigate.js
@@ -1,21 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
-class Navigate extends React.Component{
+
+function Navigate ({place,changePlace}){
+    const navigations = ['All Restaurants', 'Restaurants in Chicago', 'Restaurants in Seattle', 'Restaurants in New York']
+    return(
+        <ul className="navigate-options">
+            {navigations.map((value)=>{
+                return (
+                    <li key={value}>
+                        <button 
+                        onClick={()=>changePlace(value)} 
+                        style={value === place ? {color: 'rgb(187,36,21)'}:null} 
+                        className='btn-clr disp-none'>{value}</button>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+
+Navigate.propTypes={
+    place: PropTypes.string.isRequired,
+    changePlace: PropTypes.func.isRequired
+}
+
+export default class Information extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            place: 'All Restaurants'
+        }
+        this.changePlace = this.changePlace.bind(this)
+    }
+    changePlace(spot){
+
+        this.setState({
+            place: spot
+        })
+
+    }
     render(){
-        const navigations = ['All Restaurants', 'Restaurants in Chicago', 'Restaurants in Seattle', 'Restaurants in New York']
         return(
-            <ul className="navigate-options">
-                {navigations.map((value)=>{
-                    return (
-                        <li key={value}>
-                            <button className='btn-clr disp-none'>{value}</button>
-                        </li>
-                    )
-                })}
-            </ul>
+            <React.Fragment>
+                <Navigate place={this.state.place} changePlace={this.changePlace}/>
+            </React.Fragment>
         )
     }
 }
-
-export default Navigate
--- Application/index.js
+++ Application/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Navigate from './Components/Navigate.js'
+import Information from './Components/Navigate.js'
 
 
 class Game extends React.Component{
     render(){
         return (
-            <div className="navigation-bar"> <Navigate /> </div>
+            <div className="navigation-bar"> <Information /> </div>
         )
     }
 }
